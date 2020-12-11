42d18f0d6833ccbf5924bf8f316d68d606fcb192
hofls <hofls@ya.ru>
2020-12-11 12:42:33 +0500

    playwright. wait for logs

--- playwright/minimal-setup/src/utils/waitForLogs.js
+++ playwright/minimal-setup/src/utils/waitForLogs.js
@@ -1,7 +1,8 @@
 
 module.exports = async function waitForLogs() {
-    console.log("Without waiting - container instantly stops after tests, with empty report")
-    await sleep(1000);
+    console.log("Without waiting - container instantly stops after tests, with empty report");
+    await sleep(3000);
+    console.log("Waiting is over");
 };
 
 function sleep(ms) {
